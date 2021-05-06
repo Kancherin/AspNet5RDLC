3f9272cee71b791767fb475e178aca4953bfbfdb
David <david_horeb@hotmail.com>
2021-05-06 12:08:14 -0300

    Add Report Pdf

--- AspNet5RDLC.Web/Controllers/HomeController.cs
+++ AspNet5RDLC.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using AspNet5RDLC.Web.Models;
+using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,10 +14,12 @@ namespace AspNet5RDLC.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -30,7 +34,11 @@ namespace AspNet5RDLC.Web.Controllers
 
         public IActionResult Imprimir()
         {
-            return View();
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            var filePath = $"{_webHostEnvironment.WebRootPath}\\Reports\\rptEmpleados.rdl";
+            var local = new LocalReport(filePath);
+            var rpt = local.Execute(RenderType.Pdf);
+            return File(rpt.MainStream, "application/pdf");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
