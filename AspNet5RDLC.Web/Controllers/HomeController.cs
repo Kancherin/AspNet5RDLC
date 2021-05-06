6258021e54746e1dda6a1befa05f0e2097ed7e23
David <david_horeb@hotmail.com>
2021-05-06 12:33:20 -0300

    Datos generados

--- AspNet5RDLC.Web/Controllers/HomeController.cs
+++ AspNet5RDLC.Web/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -35,8 +36,23 @@ namespace AspNet5RDLC.Web.Controllers
         public IActionResult Imprimir()
         {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Gender");
+            dt.Columns.Add("Company");
+            dt.Columns.Add("Department");
+
+            for (int i = 1; i < 101; i++)
+            {
+                dt.Rows.Add(i, "Empleado " + i, "Male", "Administración Pública", "Desarrollo");
+            }
+
             var filePath = $"{_webHostEnvironment.WebRootPath}\\Reports\\rptEmpleados.rdl";
             var local = new LocalReport(filePath);
+
+            local.AddDataSource("DataSet1", dt);
+
             var rpt = local.Execute(RenderType.Pdf);
             return File(rpt.MainStream, "application/pdf");
         }
